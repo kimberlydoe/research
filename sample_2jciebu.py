ee5b5c71b2e3ece5efffee5a9540b01ec771946a
kimberlydoe <138801489+kimberlydoe@users.noreply.github.com>
2024-10-03 14:02:35 -0500

    Removed Flags, Turned off light

--- sample_2jciebu.py
+++ sample_2jciebu.py
@@ -7,7 +7,7 @@ import sys
 DISPLAY_RULE_NORMALLY_OFF = 0
 
 # LED display rule. Normal On.
-DISPLAY_RULE_NORMALLY_ON = 1
+DISPLAY_RULE_NORMALLY_ON = 0
 
 def s16(value):
     return -(value & 0x8000) | (value & 0x7fff)
@@ -41,26 +41,15 @@ def print_latest_data(data):
     barometric_pressure = str(int(hex(data[17]) + '{:02x}'.format(data[16], 'x')
                                   + '{:02x}'.format(data[15], 'x') + '{:02x}'.format(data[14], 'x'), 16) / 1000)
     sound_noise = str(int(hex(data[19]) + '{:02x}'.format(data[18], 'x'), 16) / 100)
-    eTVOC = str(int(hex(data[21]) + '{:02x}'.format(data[20], 'x'), 16))
-    eCO2 = str(int(hex(data[23]) + '{:02x}'.format(data[22], 'x'), 16))
+    # eTVOC = str(int(hex(data[21]) + '{:02x}'.format(data[20], 'x'), 16))
+    # eCO2 = str(int(hex(data[23]) + '{:02x}'.format(data[22], 'x'), 16))
     discomfort_index = str(int(hex(data[25]) + '{:02x}'.format(data[24], 'x'), 16) / 100)
     heat_stroke = str(s16(int(hex(data[27]) + '{:02x}'.format(data[26], 'x'), 16)) / 100)
     vibration_information = str(int(hex(data[28]), 16))
-    si_value = str(int(hex(data[30]) + '{:02x}'.format(data[29], 'x'), 16) / 10)
-    pga = str(int(hex(data[32]) + '{:02x}'.format(data[31], 'x'), 16) / 10)
-    seismic_intensity = str(int(hex(data[34]) + '{:02x}'.format(data[33], 'x'), 16) / 1000)
-    temperature_flag = str(int(hex(data[36]) + '{:02x}'.format(data[35], 'x'), 16))
-    relative_humidity_flag = str(int(hex(data[38]) + '{:02x}'.format(data[37], 'x'), 16))
-    ambient_light_flag = str(int(hex(data[40]) + '{:02x}'.format(data[39], 'x'), 16))
-    barometric_pressure_flag = str(int(hex(data[42]) + '{:02x}'.format(data[41], 'x'), 16))
-    sound_noise_flag = str(int(hex(data[44]) + '{:02x}'.format(data[43], 'x'), 16))
-    etvoc_flag = str(int(hex(data[46]) + '{:02x}'.format(data[45], 'x'), 16))
-    eco2_flag = str(int(hex(data[48]) + '{:02x}'.format(data[47], 'x'), 16))
-    discomfort_index_flag = str(int(hex(data[50]) + '{:02x}'.format(data[49], 'x'), 16))
-    heat_stroke_flag = str(int(hex(data[52]) + '{:02x}'.format(data[51], 'x'), 16))
-    si_value_flag = str(int(hex(data[53]), 16))
-    pga_flag = str(int(hex(data[54]), 16))
-    seismic_intensity_flag = str(int(hex(data[55]), 16))
+    # si_value = str(int(hex(data[30]) + '{:02x}'.format(data[29], 'x'), 16) / 10)
+    # pga = str(int(hex(data[32]) + '{:02x}'.format(data[31], 'x'), 16) / 10)
+    # seismic_intensity = str(int(hex(data[34]) + '{:02x}'.format(data[33], 'x'), 16) / 1000)
+
     print("")
     print("Time measured:" + time_measured)
     print("Temperature:" + temperature)
@@ -68,26 +57,14 @@ def print_latest_data(data):
     print("Ambient light:" + ambient_light)
     print("Barometric pressure:" + barometric_pressure)
     print("Sound noise:" + sound_noise)
-    print("eTVOC:" + eTVOC)
-    print("eCO2:" + eCO2)
+    # print("eTVOC:" + eTVOC)
+    # print("eCO2:" + eCO2)
     print("Discomfort index:" + discomfort_index)
     print("Heat stroke:" + heat_stroke)
     print("Vibration information:" + vibration_information)
-    print("SI value:" + si_value)
-    print("PGA:" + pga)
-    print("Seismic intensity:" + seismic_intensity)
-    print("Temperature flag:" + temperature_flag)
-    print("Relative humidity flag:" + relative_humidity_flag)
-    print("Ambient light flag:" + ambient_light_flag)
-    print("Barometric pressure flag:" + barometric_pressure_flag)
-    print("Sound noise flag:" + sound_noise_flag)
-    print("eTVOC flag:" + etvoc_flag)
-    print("eCO2 flag:" + eco2_flag)
-    print("Discomfort index flag:" + discomfort_index_flag)
-    print("Heat stroke flag:" + heat_stroke_flag)
-    print("SI value flag:" + si_value_flag)
-    print("PGA flag:" + pga_flag)
-    print("Seismic intensity flag:" + seismic_intensity_flag)
+    # print("SI value:" + si_value)
+    # print("PGA:" + pga)
+    # print("Seismic intensity:" + seismic_intensity)
 
 
 def now_utc_str():
